7b69c130449031f7c5cfae37b0ca5c700e924292
maxime-manicourt <maxime.manicourt@gmail.com>
2018-08-03 02:08:35 +0200

    on a bien avancé

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'static_pages#home'
+  get '/answer', to: 'static_pages#answer', as: "answer"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :user
 end
